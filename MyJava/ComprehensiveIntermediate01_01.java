e94285b2258113929079b7e978c03fc5f9224c93
syou484 <syow.y.1212@outlook.jp>
2024-12-02 01:39:49 +0900

    Add files via upload

---
+++ MyJava/ComprehensiveIntermediate01_01.java
@@ -0,0 +1,31 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ComprehensiveIntermediate01_01 {
+    public static void main(String[] agrs){
+
+        while(true){
+            System.out.println("整数を入力してください");
+        Scanner sc = new Scanner(System.in);
+
+        try{
+            int i = sc.nextInt();
+            if(i < 0){
+                System.out.println("マイナス値です");
+            } else if (i % 2 == 0){
+                System.out.println("even");  //偶数
+                break;
+            } else if (i % 2 == 1){
+                System.out.println("odd");  //奇数
+                break;
+            }
+         
+
+    } catch (InputMismatchException e) {
+        System.out.println("整数以外です");
+    }
+
+
+    }
+    }
+}
